94c814a2c0e422c4048ad74415ce0d938f49a256
David Etuh <aromeetuh@gmail.com>
2026-09-13 16:55:12 +0100

    Completed M7L1A1

---
+++ Module 7 Python Intro/L1 Intro/Activity1.py
@@ -0,0 +1,32 @@
+print("Hello World!")
+name = "David"
+print(f"My name is {name}")
+print("My name is", name)
+#Numbers
+price= 2000
+name= "Kendrick"
+print(f"{name} bought a shirt for ${price}")
+
+print("The result of 2+3:", 2+3)
+
+print(123)
+print("Line1\nLine2\nLine3")
+
+import keyword
+print(keyword.kwlist)
+
+food = "Chicken Pizza"
+drink ="Coke"
+print(f"My favorite food is {food} and my favorite drink is {drink}")
+
+print(123*678)
+
+print(f"65*1: {65*1}")
+print(f"65*2: {65*2}")
+print(f"65*3: {65*3}")
+
+a="abcd"
+b="efgh"
+print(a+b)
+
+print("Hello"*5)
