966d373a03b2a1c2289023ef17aedccd9c3e9a1b
David Etuh <aromeetuh@gmail.com>
2026-09-13 18:40:12 +0100

    Completed M7P1

---
+++ Module 7 Python Intro/L1 Intro/M7P1.py
@@ -0,0 +1,13 @@
+up = "6:30 am"
+prep = "Brush, Bath, and wear new clothes"
+chores = "sweeping, doing dishes"
+activity = "school, homework"
+eat = "food"
+later = "rest"
+
+print(f"Wake up by {up}")
+print(f"Prepare and get ready to {prep}")
+print(f"Do the chores by {chores}")
+print(f"Start the day by eating {eat}")
+print(f"Kick-off the activities by focusing on {activity}")
+print(f"After the day's activity, come back and {later}")
